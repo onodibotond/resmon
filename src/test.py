5fd5496db9032e31b62cfd91057f9bb14370afcd
Onodi Botond <botond.onodi@microfocus.com>
2020-02-06 00:28:43 +0200

    fixing tests

--- src/test.py
+++ src/test.py
@@ -4,9 +4,6 @@ import os
 
 class TestStringMethods(unittest.TestCase): 
     
-    self.test_path = '/tmp/test_resmon'
-    self.test_file = '/tmp/test_resmon/test.tar.gz'
-
     def test_process_monitor(self): 
         self.assertBetween(monitor.get_ram_percent(), 0, 100) 
   
@@ -14,19 +11,22 @@ class TestStringMethods(unittest.TestCase):
         self.assertRaises(monitor.make_tarfile("/inexistent/path/to/test/test.tar.gz", "/inexistent/path/to/test"), FileNotFoundError)
   
     def test_make_tarfile(self):
-        os.mkdir(self.test_path)
-        monitor.make_tarfile(self.test_file, self.test_path)
-        self.assertTrue(os.path.exists(self.test_file))
-        os.rmdir(self.test_path)
+        test_path = '/tmp/test_resmon'
+        test_file = '/tmp/test_resmon/test.tar.gz'
+        os.mkdir(test_path)
+        monitor.make_tarfile(test_file, test_path)
+        self.assertTrue(os.path.exists(test_file))
+        os.rmdir(test_path)
     
     def test_clean_old_files(self):
-        os.mkdir(self.test_path)
-        monitor.make_tarfile(self.test_file, self.test_path)
-        monitor.clean_old_files(0, 0, self.test_file)
-        self.assertTrue(os.path.exists(self.test_file))
-        os.rmdir(self.test_path)
+        test_path = '/tmp/test_resmon'
+        test_file = '/tmp/test_resmon/test.tar.gz'
+        os.mkdir(test_path)
+        monitor.make_tarfile(test_file, test_path)
+        monitor.clean_old_files(0, 0, test_file)
+        self.assertTrue(os.path.exists(test_file))
+        os.rmdir(test_path)
         
- 
   
 if __name__ == '__main__': 
     unittest.main() 
