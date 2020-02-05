18cc8ab049347cf9d0a22c30295662e24301aa30
Onodi Botond <botond.onodi@microfocus.com>
2020-02-06 00:24:01 +0200

    fixing unit test

--- src/test.py
+++ src/test.py
@@ -4,9 +4,8 @@ import os
 
 class TestStringMethods(unittest.TestCase): 
     
-    def __init__(self):
-        self.test_path = '/tmp/test_resmon'
-        self.test_file = '/tmp/test_resmon/test.tar.gz'
+    self.test_path = '/tmp/test_resmon'
+    self.test_file = '/tmp/test_resmon/test.tar.gz'
 
     def test_process_monitor(self): 
         self.assertBetween(monitor.get_ram_percent(), 0, 100) 
@@ -15,10 +14,10 @@ class TestStringMethods(unittest.TestCase):
         self.assertRaises(monitor.make_tarfile("/inexistent/path/to/test/test.tar.gz", "/inexistent/path/to/test"), FileNotFoundError)
   
     def test_make_tarfile(self):
-        os.mkdir(path)
+        os.mkdir(self.test_path)
         monitor.make_tarfile(self.test_file, self.test_path)
         self.assertTrue(os.path.exists(self.test_file))
-        os.rmdir(path)
+        os.rmdir(self.test_path)
     
     def test_clean_old_files(self):
         os.mkdir(self.test_path)
